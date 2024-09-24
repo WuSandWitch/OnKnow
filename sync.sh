b74ead0a12e6073092f2d0c28aab48ac7c12b8ba
賽博農民工阿溟 <WuSandWitch@gmail.com>
2024-09-24 14:38:49 +0800

    Quartz sync: Sep 24, 2024, 2:38 PM

--- sync.sh
+++ sync.sh
@@ -1,5 +1,5 @@
 echo "Sync started at $(date)" >> sync.log
 npm run sync-notes
 npx quartz sync
-echo "Sync & Deplay Completed at $(date)"
+echo "Sync & Deplay Completed at $(date)" >> sync.log
 echo "url: https://wusandwitch.zudo.cc/OnKnow"
